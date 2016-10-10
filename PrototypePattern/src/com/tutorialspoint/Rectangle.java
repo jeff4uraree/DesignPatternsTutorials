7c6b80f9ee1706ce6f20e895d622167acef5b1c7
jeff4uraree <jeff4uraree@gmail.com>
2016-10-10 18:43:15 -0400

    Finished prototype design pattern example

---
+++ PrototypePattern/src/com/tutorialspoint/Rectangle.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint;
+
+public class Rectangle extends Shape{
+
+	public Rectangle() {
+		type = "Rectangle";
+	}
+
+	@Override
+	void draw() {
+		System.out.println("Inside Square::draw() method");
+	}
+	
+	
+}
